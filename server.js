c2d677949bb4d50e3bef354fb9e5684d06248ca0
ahmedmoha79 <153353444+ahmedmoha79@users.noreply.github.com>
2025-02-19 20:07:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://mohamedbeyhaqi:bQJx9JfwQNlJEXOE@cluster0.eu65h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected'))
